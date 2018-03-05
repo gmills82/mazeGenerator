4e491227a069f17ddaecd203191f42c09e96e89d
Grant Mills <grant.mills@bodybuilding.com>
2018-03-04 22:00:13 -0500

    linking and unlinking methods added to cells as well as neighbors

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,3 +1,5 @@
+import java.util.EnumMap;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -12,10 +14,61 @@ public class Cell {
 	private Cell south;
 	private Cell east;
 	private Cell west;
+	private EnumMap<Direction, Cell> neighbors;
 
 	public Cell(Integer row, Integer column) {
 		this.row = row;
 		this.column = column;
+		this.linkedCells = new HashSet<>();
+
+		this.neighbors = new EnumMap<Direction, Cell>(Direction.class);
+	}
+
+	//Linked cells are ones that share a passage
+	//Links two cells, bidirectionally is the flag is true
+	private void link(Cell cellToBeLinked, Boolean isBiDirectional) {
+		//Default value
+		if(isBiDirectional == null) {
+			isBiDirectional = true;
+		}
+		this.linkedCells.add(cellToBeLinked);
+		if(isBiDirectional) {
+			Set<Cell> currentlyLinked = cellToBeLinked.getLinkedCells();
+			if(!currentlyLinked.contains(this)) {
+				currentlyLinked.add(this);
+				cellToBeLinked.setLinkedCells(currentlyLinked);
+			}
+		}
+	}
+
+	//Unlinks two cells, bidirectionally if the flag is true
+	private void unlink(Cell cellToBeUnlinked, Boolean isBiDirectional) {
+		//Default value
+		if(isBiDirectional == null) {
+			isBiDirectional = true;
+		}
+		if(this.linkedCells.contains(cellToBeUnlinked)) {
+			this.linkedCells.remove(cellToBeUnlinked);
+		}
+		if(isBiDirectional) {
+			Set<Cell> currentlyLinked = cellToBeUnlinked.getLinkedCells();
+			if(currentlyLinked.contains(this)) {
+				currentlyLinked.remove(this);
+				cellToBeUnlinked.setLinkedCells(currentlyLinked);
+			}
+		}
+	}
+
+	public Boolean isLinked(Cell testCell) {
+		return this.getLinkedCells().contains(testCell);
+	}
+
+	public Set<Cell> getLinkedCells() {
+		return linkedCells;
+	}
+
+	public void setLinkedCells(Set<Cell> linkedCells) {
+		this.linkedCells = linkedCells;
 	}
 
 	public Integer getRow() {
@@ -32,6 +85,7 @@ public class Cell {
 
 	public void setNorth(Cell north) {
 		this.north = north;
+		this.neighbors.put(Direction.NORTH, north);
 	}
 
 	public Cell getSouth() {
@@ -40,6 +94,7 @@ public class Cell {
 
 	public void setSouth(Cell south) {
 		this.south = south;
+		this.neighbors.put(Direction.SOUTH, south);
 	}
 
 	public Cell getEast() {
@@ -48,6 +103,7 @@ public class Cell {
 
 	public void setEast(Cell east) {
 		this.east = east;
+		this.neighbors.put(Direction.EAST, east);
 	}
 
 	public Cell getWest() {
@@ -56,5 +112,10 @@ public class Cell {
 
 	public void setWest(Cell west) {
 		this.west = west;
+		this.neighbors.put(Direction.WEST, west);
+	}
+
+	public EnumMap<Direction, Cell> getNeighbors() {
+		return neighbors;
 	}
 }
---
+++ src/main/java/Direction.java
@@ -0,0 +1,7 @@
+/**
+ * @author Grant Mills
+ * @since 3/4/18
+ */
+public enum Direction {
+	NORTH, SOUTH, EAST, WEST
+}
